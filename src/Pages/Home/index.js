69e9c000d104161843c8b21658059b688ec21913
Andrey Bejarano <andreybejarano@gmail.com>
2020-08-16 21:34:38 -0300

    feat(home): get lists and show with CarouselList component

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import CarouselList from 'Components/CarouselList';
+import Loader from 'Components/Loader';
+
+import { getAllLists } from '@/store/actions/list';
+
 import styles from './Home.css';
 
 class Home extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(getAllLists());
+  }
+
   render() {
+    const { lists } = this.props;
     return (
       <div className={styles.root}>
-        <h1 variant="h1">Home</h1>
+        {lists.isFetching &&
+          <div className={styles.containerLoader}>
+            <Loader></Loader>
+          </div>
+        }
+        {lists.success &&
+          lists.data.map((item, key) =>
+            item.list && <CarouselList list={item.list} key={key}></CarouselList>
+          )
+        }
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
+  lists: state.list.all
 });
 
 export default connect(mapStateToProps)(Home);
