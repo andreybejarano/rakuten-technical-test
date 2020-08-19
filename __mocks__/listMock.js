90b115f36b77069c01a21c00cf6f379cb188fb7d
Andrey Bejarano <andreybejarano@gmail.com>
2020-08-19 00:24:27 -0300

    feat: create jesto configuration

---
+++ __mocks__/listMock.js
@@ -0,0 +1,257 @@
+module.exports = {
+  type: 'lists',
+  id: 'nuestras-preferidas-de-la-semana',
+  numerical_id: 4334,
+  category: 'svod',
+  short_name: 'Nuestras prefes',
+  content_type: 'Movie',
+  is_b2b: false,
+  wktv_code: null,
+  only_coupon: false,
+  is_recommendation: false,
+  name: 'Nuestras preferidas de la semana',
+  additional_images: {},
+  kind: 'regular',
+  contents: {
+    data: [
+      {
+        type: 'movies',
+        id: 'rompe-ralph',
+        numerical_id: 3406,
+        title: '¡Rompe Ralph!',
+        year: 2012,
+        duration: 97,
+        label: 'RTV Suscripción',
+        classification: {
+          type: 'classifications',
+          id: '1',
+          numerical_id: 1,
+          name: 'TP',
+          age: 0,
+          adult: false,
+          description: 'Apta para todos los públicos',
+          default: false
+        },
+        images: {
+          artwork: 'https://images-3.wuaki.tv/system/artworks/3406/master/rompe-ralph-1558429133.jpeg',
+          snapshot: 'https://images-2.wuaki.tv/system/shots/196368/original/snapshot-1590665215.jpeg',
+          ribbons: []
+        },
+        highlighted_score: {
+          score: 7.9,
+          amount_of_votes: null,
+          formatted_amount_of_votes: null,
+          site: {
+            type: 'sites',
+            id: '1',
+            numerical_id: 1,
+            name: 'IMDb',
+            show_image: false,
+            scale: 10,
+            image: 'https://images-0.wuaki.tv/system/images/1/original/imdb-1594119387.png'
+          }
+        },
+        rating: {
+          average: 4,
+          scale: 5
+        },
+        labels: {
+          audio_qualities: [
+            {
+              type: 'audio_qualities',
+              id: '2.0',
+              numerical_id: 2,
+              name: '2.0 (Stereo)',
+              abbr: '2.0',
+              image: 'https://images-1.wuaki.tv/system/logos/2/original/2-0-stereo-1456155689.png'
+            }
+          ],
+          hdr_types: [
+            {
+              type: 'hdr_types',
+              id: 'NONE',
+              numerical_id: 1,
+              name: 'SDR',
+              abbr: 'NONE',
+              image: null
+            }
+          ],
+          purchase_types: [
+            {
+              type: 'purchase_types',
+              id: '1',
+              numerical_id: 1,
+              is_recurring: false,
+              name: 'Alquiler 48H',
+              label:
+                'ALQUILER 48H',
+              kind: 'rental',
+              expires_after_in_seconds: 172800,
+              available_time_in_seconds: 172800
+            },
+            {
+              type: 'purchase_types',
+              id: '2',
+              numerical_id: 2,
+              is_recurring: false,
+              name: 'Venta (Digital Locker)',
+              label: 'VENTA (DIGITAL LOCKER)',
+              kind: 'purchase',
+              expires_after_in_seconds: null,
+              available_time_in_seconds: null
+            },
+            {
+              type: 'purchase_types',
+              id: '4',
+              numerical_id: 4,
+              is_recurring: true,
+              name: 'Suscripción',
+              label: 'SUSCRIPCIóN',
+              kind: 'subscription',
+              expires_after_in_seconds: null,
+              available_time_in_seconds: null
+            }
+          ],
+          video_qualities: [
+            {
+              type: 'video_qualities',
+              id: 'SD',
+              numerical_id: 2,
+              name: 'SD',
+              abbr: 'SD',
+              position: 0,
+              image: 'https://images-1.wuaki.tv/system/logos/2/original/sd-1456155688.png'
+            },
+            {
+              type: 'video_qualities',
+              id: 'HD',
+              numerical_id: 1,
+              name: 'HD',
+              abbr: 'HD',
+              position: 1,
+              image: 'https://images-0.wuaki.tv/system/logos/1/original/hd-1456155687.png'
+            }
+          ]
+        }
+      },
+      {
+        type: 'movies',
+        id: 'sobrevive-esta-noche',
+        numerical_id: 127099,
+        title: 'Sobrevive esta noche',
+        year: 2020,
+        duration: 86,
+        label: '3,99 €',
+        classification: {
+          type: 'classifications',
+          id: '5',
+          numerical_id: 5,
+          name: '18',
+          age: 18,
+          adult:
+            false,
+          description: 'Mostrar todos los contenidos, excluyendo los clasificados para adultos',
+          default: true
+        },
+        images: {
+          artwork: 'https://images-2.wuaki.tv/system/artworks/127099/master/sobrevive-esta-noche-1594366830.png',
+          snapshot: 'https://images-0.wuaki.tv/system/shots/224078/original/survive-the-night-1594046326.jpeg',
+          ribbons: []
+        },
+        highlighted_score: {
+          score: 4.2,
+          amount_of_votes: 50,
+          formatted_amount_of_votes: '50',
+          site: {
+            type: 'sites',
+            id: '1',
+            numerical_id: 1,
+            name: 'IMDb',
+            show_image: false,
+            scale: 10,
+            image: 'https://images-0.wuaki.tv/system/images/1/original/imdb-1594119387.png'
+          }
+        },
+        rating: {
+          average: 3,
+          scale: 5
+        },
+        labels: {
+          audio_qualities: [
+            {
+              type: 'audio_qualities',
+              id: '2.0',
+              numerical_id: 2,
+              name: '2.0 (Stereo)',
+              abbr: '2.0',
+              image: 'https://images-1.wuaki.tv/system/logos/2/original/2-0-stereo-1456155689.png'
+            }
+          ],
+          hdr_types: [
+            {
+              type: 'hdr_types',
+              id: 'NONE',
+              numerical_id: 1,
+              name: 'SDR',
+              abbr: 'NONE',
+              image: null
+            }
+          ],
+          purchase_types: [
+            {
+              type: 'purchase_types',
+              id: '1',
+              numerical_id: 1,
+              is_recurring: false,
+              name: 'Alquiler 48H',
+              label: 'ALQUILER 48H',
+              kind: 'rental',
+              expires_after_in_seconds: 172800,
+              available_time_in_seconds: 172800
+            },
+            {
+              type: 'purchase_types',
+              id: '2',
+              numerical_id: 2,
+              is_recurring: false,
+              name: 'Venta (Digital Locker)',
+              label: 'VENTA (DIGITAL LOCKER)',
+              kind: 'purchase',
+              expires_after_in_seconds: null,
+              available_time_in_seconds: null
+            }
+          ],
+          video_qualities: [
+            {
+              type: 'video_qualities',
+              id: 'SD',
+              numerical_id: 2,
+              name: 'SD',
+              abbr: 'SD',
+              position: 0,
+              image: 'https://images-1.wuaki.tv/system/logos/2/original/sd-1456155688.png'
+            },
+            {
+              type: 'video_qualities',
+              id: 'HD',
+              numerical_id: 1,
+              name: 'HD',
+              abbr: 'HD',
+              position: 1,
+              image: 'https://images-0.wuaki.tv/system/logos/1/original/hd-1456155687.png'
+            }
+          ]
+        }
+      }
+    ],
+    meta: {
+      pagination: {
+        page: 1,
+        count: 1,
+        per_page: 18,
+        offset: 0,
+        total_pages: 1
+      }
+    }
+  }
+};
